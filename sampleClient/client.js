cddc5e6f754a6e6797445392049f3914e3f9c867
Cayle Sharrock <cayle@coinbase.com>
2018-01-25 22:08:46 +0200

    Add SSL support

--- sampleClient/client.js
+++ sampleClient/client.js
@@ -4,9 +4,12 @@
 const services = require('../build/proto/session_grpc_pb');
 const messages = require('../build/proto/session_pb');
 const grpc = require('grpc');
+const certs = require("../lib/certs");
 
 function main() {
-    const client = new services.SessionClient('localhost:50051', grpc.credentials.createInsecure());
+    const keyPair = certs.loadKeyPair('client');
+    const creds = grpc.credentials.createSsl(certs.loadCA(), keyPair.private_key, keyPair.cert_chain);
+    const client = new services.SessionClient('localhost:50051', creds);
     const req = new messages.SessionCreateRequest();
     client.sessionCreate(req, (err, res) => {
         console.log(err);
--- server/server.js
+++ server/server.js
@@ -4,26 +4,36 @@
 const services = require('../build/proto/session_grpc_pb');
 const messages = require('../build/proto/session_pb');
 const grpc = require('grpc');
+const certs = require('../lib/certs');
 
 /**
  * Implements the SayHello RPC method.
  */
 function createSession(call, callback) {
-    var reply = new messages.SessionCreateResponse();
+    const reply = new messages.SessionCreateResponse();
     reply.setSessionId('1234567abcd');
     callback(null, reply);
 }
+
+
 
 /**
  * Starts an RPC server that receives requests for the Greeter service at the
  * sample server port
  */
-function main() {
-    var server = new grpc.Server();
-    server.addService(services.SessionService, {sessionCreate: createSession});
-    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-    server.start();
-    console.log('Listening on port 50051');
+async function main() {
+    try {
+        const keyPair = await certs.loadKeyPair('server');
+        const ssl_creds = grpc.ServerCredentials.createSsl(null, [keyPair], true);
+        const server = new grpc.Server();
+        server.addService(services.SessionService, {sessionCreate: createSession});
+        server.bind('0.0.0.0:50051', ssl_creds);
+        server.start();
+        console.log('Listening on port 50051');
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
 }
 
 main();
