b1f03207504684a35c0ff73b646d809e2505c821
Cayle Sharrock <cayle@nimbustech.biz>
2018-01-26 09:33:00 +0200

    Some tidying up in demo

--- sampleClient/client.js
+++ sampleClient/client.js
@@ -5,17 +5,30 @@ const services = require('../build/proto/session_grpc_pb');
 const messages = require('../build/proto/session_pb');
 const grpc = require('grpc');
 const certs = require("../lib/certs");
+const logger = require('../lib/logging');
+
+function exit(msg, code = 0) {
+    const level = code === 0 ? 'info' : 'error';
+    logger.log(level, msg);
+    process.exit(code);
+}
 
 function main() {
     const keyPair = certs.loadKeyPair('client');
+    const CA = certs.loadCA();
+    if (!keyPair || !CA) {
+        exit('Could not load certificates. Exiting.', 1);
+        return;
+    }
     const creds = grpc.credentials.createSsl(certs.loadCA(), keyPair.private_key, keyPair.cert_chain);
     const client = new services.SessionClient('localhost:50051', creds);
     const req = new messages.SessionCreateRequest();
     client.sessionCreate(req, (err, res) => {
-        console.log(err);
-        console.log(res.getSessionId());
+        err && logger.log(err);
+        logger.info({ message: 'Session created', session: res });
+        logger.info(res.getSessionId());
+        exit('Thanks for playing')
     });
-
 }
 
 main();
