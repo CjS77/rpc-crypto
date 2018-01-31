536bee4553039b7f15602453095bfdd70b08684d
Cayle Sharrock <cayle@nimbustech.biz>
2018-01-31 19:46:30 +0200

    Update client

--- sampleClient/client.js
+++ sampleClient/client.js
@@ -1,14 +1,13 @@
 /* jshint node:true */
 'use strict';
 
-import {exit} from '../lib/logging';
-const services = require('../build/node/exchange_grpc_pb');
-const messages = require('../build/node/exchange_pb');
+const {exit, logger} = require('../lib/logging');
 const grpc = require('grpc');
 const certs = require('../lib/certs');
-const logger = require('../lib/logging');
+const path = require('path');
 
-function main() {
+async function main() {
+    const exchange = await grpc.load({file: 'exchange.proto', root: path.resolve(__dirname, '../proto')});
     const keyPair = certs.loadKeyPair('client');
     const CA = certs.loadCA();
     if (!keyPair || !CA) {
@@ -16,14 +15,12 @@ function main() {
         return;
     }
     const creds = grpc.credentials.createSsl(certs.loadCA(), keyPair.private_key, keyPair.cert_chain);
-    const client = new services.SessionClient('localhost:50051', creds);
-    const req = new messages.SessionCreateRequest();
-    client.getExchangesList(req, (err, res) => {
-        err && logger.log(err);
-        logger.info({message: 'Exchanges', exchanges: res});
-        logger.info(res.getVersion());
-        logger.info(res.getExchangesList());
-        exit('Thanks for playing');
+    const client = new exchange.rpc_crypto.Exchange('localhost:50051', creds);
+    client.getExchangeIDs({}, (err, res) => {
+        if (err) {
+            logger.error(err);
+        }
+        logger.info('IDs', res);
     });
 }
 
